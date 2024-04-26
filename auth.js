cbbf882ff53f6d8b4cbd9bddc0fdd58d52c1740f
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2024-04-26 10:28:31 +0530

    added callback for google auth

--- auth.js
+++ auth.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID:   process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback",
+    //local Callback
+    // callbackURL: "http://localhost:8080/auth/google/callback",
+    //hosting callback
+    callbackURL: "https://maketrip-in.onrender.com/listings/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
